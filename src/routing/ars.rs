1e2e64310ec9b5091ab9eed6b8dfd18d97f0b9c0
Andeya <andeyalee@outlook.com>
2024-01-13 12:06:08 +0800

    feat: add plugin
    Change-Id: I775a7abc65958c6b6b0168c52b8fc40d9d8b99c4

--- src/routing/ars.rs
+++ src/routing/ars.rs
@@ -1,3 +1,7 @@
+use axum::http::Request;
+use std::task::{Context, Poll};
+use tower::Service;
+
 use crate::error::*;
 use crate::{
     ars::{Ars, Method},
@@ -40,16 +44,57 @@ impl GwRouter {
                     })
                     .context(ArsSnafu)?;
                 let service = Arc::new(service.clone());
-                router = router.route(
-                    location.path.as_str(),
-                    top_level_handler_fn!(location.method, || async move {
-                        // TODO:
-                        format!("{:?}", service)
-                    }),
-                );
+                router = router
+                    .route(
+                        location.path.as_str(),
+                        top_level_handler_fn!(location.method, || async move {
+                            // TODO:
+                            format!("{:?}", service)
+                        }),
+                    )
+                    .route_layer(PluginRouteLayer);
             }
             gw_router = gw_router.route(ingress_domain_group.domain_name, router);
         }
         Ok(gw_router)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct PluginRouteLayer;
+
+#[derive(Debug, Clone)]
+struct PluginRouteService<S> {
+    inner: S,
+}
+
+impl<S> tower::Layer<S> for PluginRouteLayer {
+    type Service = PluginRouteService<S>;
+
+    // 初始化服务
+    fn layer(&self, inner: S) -> Self::Service {
+        PluginRouteService { inner }
+    }
+}
+
+impl<ResBody, S> Service<Request<ResBody>> for PluginRouteService<S>
+where
+    S: Service<Request<ResBody>>,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = S::Future;
+
+    #[inline]
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    #[allow(unused_mut)]
+    fn call(&mut self, mut req: Request<ResBody>) -> Self::Future {
+        println!("[PLUGIN-PRE] Handling a request to {}", req.uri());
+        let resp = self.inner.call(req);
+        println!("[PLUGIN-POST] Returning a response");
+        resp
     }
 }
