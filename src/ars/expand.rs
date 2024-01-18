8b6f3ec39c28bdd0a34495a50e286d75d363ff49
Andeya <andeyalee@outlook.com>
2024-01-19 01:15:26 +0800

    chore: add ArsExpand
    Change-Id: Id132f6a3258a76664142c29068e74a97e4928c18

--- src/ars/expand.rs
+++ src/ars/expand.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, sync::Arc};
 
-use crate::error::GwError;
+use crate::error::*;
 
 use super::{Ars, Domain, HeaderName, Method, MethodSpec, Namespace, ServiceSpec};
 
@@ -33,6 +33,63 @@ impl TryFrom<Ars> for ArsExpand {
     type Error = GwError;
 
     fn try_from(value: Ars) -> Result<Self, Self::Error> {
-        todo!()
+        let namespace = value.namespace;
+        let mut domain_groups = HashMap::new();
+        let services = value.egress.services;
+        for (domain, domain_group) in value.ingress.domain_groups {
+            let mut locations = Vec::new();
+
+            for location in domain_group.locations {
+                let id = location.id;
+                let path = location.path;
+                let method = location.method;
+                let proxy_hide_headers = location.proxy_hide_headers;
+                let proxy_pass_headers = location.proxy_pass_headers;
+                let upstream_service = services
+                    .get(&location.upstream_service_id)
+                    .context(NoUpstreamSnafu {
+                        id: location.upstream_service_id,
+                    })
+                    .context(ArsSnafu)?
+                    .clone();
+
+                let upstream_method = if let Some(upstream_method_id) = location.upstream_method_id {
+                    Some(
+                        upstream_service
+                            .methods
+                            .get(&upstream_method_id)
+                            .context(NoUpstreamSnafu { id: upstream_method_id })
+                            .context(ArsSnafu)?
+                            .clone(),
+                    )
+                } else {
+                    None
+                };
+
+                let ingress_location_spec = IngressLocationSpec {
+                    id,
+                    path,
+                    method,
+                    proxy_hide_headers,
+                    proxy_pass_headers,
+                    upstream_service: Arc::new(upstream_service),
+                    upstream_method: Arc::new(upstream_method),
+                };
+
+                locations.push(ingress_location_spec);
+            }
+
+            let ingress_domain_group_expand = IngressDomainGroupExpand {
+                domain_name: domain.clone(),
+                locations,
+            };
+
+            domain_groups.insert(domain, ingress_domain_group_expand);
+        }
+
+        Ok(ArsExpand {
+            namespace,
+            domain_groups,
+        })
     }
 }
--- src/routing/ars.rs
+++ src/routing/ars.rs
@@ -1,4 +1,4 @@
-use crate::error::*;
+use crate::ars::ArsExpand;
 use crate::plugin::layer;
 use crate::{
     ars::{Ars, Method},
@@ -6,7 +6,6 @@ use crate::{
     routing::GwRouter,
     routing::Router,
 };
-use std::sync::Arc;
 
 macro_rules! top_level_handler_fn {
     ($method:expr, $handler:expr) => {
@@ -26,26 +25,18 @@ macro_rules! top_level_handler_fn {
 
 impl GwRouter {
     pub(crate) fn from_ars(ars: Ars) -> Result<GwRouter, GwError> {
+        let ars: ArsExpand = ars.try_into()?;
         let mut gw_router = GwRouter::new();
         println!("namespace: {}", &*ars.namespace);
-        for (_, ingress_domain_group) in ars.ingress.domain_groups {
+        for (_, ingress_domain_group) in ars.domain_groups {
             println!("domain_name: {}", &*ingress_domain_group.domain_name);
             let mut router = Router::new();
             for location in ingress_domain_group.locations {
-                let service = ars
-                    .egress
-                    .services
-                    .get(&location.upstream_service_id)
-                    .context(NoUpstreamSnafu {
-                        id: location.upstream_service_id,
-                    })
-                    .context(ArsSnafu)?;
-                let service = Arc::new(service.clone());
                 router = router.route(
                     location.path.as_str(),
                     top_level_handler_fn!(location.method, || async move {
                         // TODO:
-                        let s = format!("{:?}", service);
+                        let s = format!("{:?}", location.upstream_service);
                         println!("response: {:?}", s);
                         s
                     }),
