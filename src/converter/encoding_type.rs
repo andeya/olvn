92a1b9c1b3c2ac0e7518d7a7ff7a12d615b1fffe
Andeya <andeyalee@outlook.com>
2024-01-20 01:22:39 +0800

    Update encoding_type.rs
    Change-Id: Ib3e5b7c3860e20a5f90481ea8a0ae2bbf31148de

--- src/converter/encoding_type.rs
+++ src/converter/encoding_type.rs
@@ -10,7 +10,10 @@ impl EncodingType {
     const TEXT_PLAIN: EncodingType = EncodingType(5u8);
     const TEXT_PROTOBUF: EncodingType = EncodingType(6u8);
 
-    pub(crate) fn from_request<F: Fn(&Request) -> Self>(req: &Request, fallback: F) -> Self {
+    pub(crate) fn from_request<F: Fn(&Request) -> Option<Self>>(req: &Request, main_mapping: F) -> Self {
+        if let Some(et) = main_mapping(req) {
+            return et;
+        }
         if let Some(h) = req.headers().get("Content-Type") {
             match h.as_bytes().splitn(2, |b| *b == b';').next() {
                 Some(h) if h == b"application/json" => EncodingType::JSON,
@@ -19,10 +22,10 @@ impl EncodingType {
                 Some(h) if h == b"text/plain" => EncodingType::TEXT_PLAIN,
                 Some(h) if h == b"application/x-protobuf" => EncodingType::TEXT_PROTOBUF,
                 Some(h) if h == b"text/html" => EncodingType::TEXT_HTML,
-                _ => fallback(req),
+                _ => Self::NO_CARE,
             }
         } else {
-            fallback(req)
+            Self::NO_CARE
         }
     }
 }
